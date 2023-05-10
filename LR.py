56ec3a57aa3e07c26efe14cd8bdd7fd46fb699a7
Maximillainy <maxillian4@gmail.com>
2023-05-10 11:37:47 -0700

    Encode string val so model can use the data

--- LR.py
+++ LR.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
 
 
 def main():
@@ -12,7 +13,23 @@ def main():
     print(data)
     # x is the input set, y is the output set
     x = data.drop(columns=['price', 'id', 'url', 'region_url', 'VIN', 'image_url', 'description', 'county', 'lat', 'long'])
-    x = pd.get_dummies(x)
+    encoder = LabelEncoder()
+    x['region'] = encoder.fit_transform(x['region'].astype(str))
+    x['manufacturer'] = encoder.fit_transform(x['manufacturer'].astype(str))
+    x['model'] = encoder.fit_transform(x['model'].astype(str))
+    x['condition'] = encoder.fit_transform(x['condition'].astype(str))
+    x['cylinders'] = encoder.fit_transform(x['cylinders'].astype(str))
+    x['fuel'] = encoder.fit_transform(x['fuel'].astype(str))
+    x['title_status'] = encoder.fit_transform(x['title_status'].astype(str))
+    x['transmission'] = encoder.fit_transform(x['transmission'].astype(str))
+    x['drive'] = encoder.fit_transform(x['drive'].astype(str))
+    x['size'] = encoder.fit_transform(x['size'].astype(str))
+    x['type'] = encoder.fit_transform(x['type'].astype(str))
+    x['paint_color'] = encoder.fit_transform(x['paint_color'].astype(str))
+    x['state'] = encoder.fit_transform(x['state'].astype(str))
+    x['posting_date'] = encoder.fit_transform(x['posting_date'].astype(str))
+    imputer = SimpleImputer(strategy='mean')
+    x = imputer.fit_transform(x)
     y = data['price']
 
     print("\nX: ")
